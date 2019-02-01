cf41dc591d4cb348d89f0c0c683d67c85318e12c
mnptrinity <45782840+mnptrinity@users.noreply.github.com>
2019-02-01 13:49:41 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,4 +2,4 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors',TRUE);
-$con=mysqli_connect('localhost','root','Mohan@007','Sakthi');
+$con=mysqli_connect('localhost','root','','Sakthi');
